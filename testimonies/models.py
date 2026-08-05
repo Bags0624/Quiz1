c9234a5dfe04d2704a58f19e8eb6af17cf06ec40
Bags0624 <rlbagaporo@student.hau.edu.ph>
2026-08-05 23:55:34 +0800

    Added testimony detail view and navbar link

--- testimonies/models.py
+++ testimonies/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Testimony(models.Model):
     author = models.CharField(max_length=100)
     content = models.TextField()
+    project = models.CharField(max_length=200, blank=True, null=True)
+    contact = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Testimony by {self.author}"
+        return self.author
--- testimonies/views.py
+++ testimonies/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Testimony
 
 def testimony_list(request):
     testimonies = Testimony.objects.all().order_by('-created_at')
     return render(request, 'testimonies/testimony_list.html', {'testimonies': testimonies})
+
+def testimony_detail(request, pk):
+    testimony = get_object_or_404(Testimony, pk=pk)
+    return render(request, 'testimonies/testimony_detail.html', {'testimony': testimony})
